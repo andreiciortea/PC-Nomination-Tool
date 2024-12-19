20f8e049dea60962d0aa21a4107bef0b2920d423
Andrei Ciortea <andrei.ciortea@gmail.com>
2024-12-20 00:56:32 +0100

    feat: update content for ECAI-2025

--- pc_nomination/choices.py
+++ pc_nomination/choices.py
@@ -1,23 +1,24 @@
 AREA_CHOICES = [
-    ("fairness_ethics_trust", "Fairness, Ethics, and Trust"),
     ("computer_vision", "Computer Vision"),
     ("constraints_satisfiability", "Constraints and Satisfiability"),
     ("data_mining", "Data Mining"),
+    ("fairness_ethics_trust", "Fairness, Ethics, and Trust"),
+    ("game_theory_economic_paradigms", "Game Theory and Economic Paradigms"),
     ("humans_ai", "Humans and AI"),
+    ("intelligent_robotics", "Intelligent Robotics"),
     ("knowledge_representation_reasoning", "Knowledge Representation and Reasoning"),
     ("machine_learning", "Machine Learning"),
     ("multiagent_systems", "Multiagent Systems"),
     ("natural_language_processing", "Natural Language Processing"),
     ("planning_search", "Planning and Search"),
-    ("robotics", "Robotics"),
     ("uncertainty_in_ai", "Uncertainty in AI"),
     ("multidisciplinary_topics", "Multidisciplinary Topics"),
 ]
 
 PHD_DATE_CHOICES = [
     ("pre-2020", "pre-2020"),
-    ("2020-2023", "2020-2023"),
-    ("2024", "2024 (Jan-Apr)"),
+    ("2020-2024", "2020-2024"),
+    ("2025", "2025 (Jan-Apr)"),
 ]
 
 IMPORT_ACTIONS_CHOICES = [
